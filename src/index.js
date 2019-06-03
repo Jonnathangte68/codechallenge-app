0e78c407d4a188dbb51246128145424090559a4b
Jonnathan Guarate <jguarate@arkusnexus.com>
2019-06-03 11:31:59 -0700

    some changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom";
+import { gql } from "apollo-boost";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import './Main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+*/
+
+const App = () => (
+  <ApolloProvider client={client}>
+      <div>
+            <div>
+                <p>Hacker News</p>
+            </div>
+            <div>
+                <a> | new</a>
+                <a> | past</a>
+                <a> | comments</a>
+                <a> | ask</a>
+                <a> | show</a>
+                <a> | jobs</a>
+                <a> | submit</a>
+            </div>
+      </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"));
